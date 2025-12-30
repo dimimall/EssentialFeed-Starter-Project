f3b1b2070a88ba1579b3073fae49b00e2a85b144
dimimall <malliaroy@gmail.com>
2025-12-30 17:55:32 +0200

    Extract duplicate test helpers into a shared scope

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/FeedCacheTestHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  FeedCacheTestHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Dimitra Malliarou on 30/12/25.
+//
+
+import Foundation
---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  SharedTestHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Dimitra Malliarou on 30/12/25.
+//
+
+import Foundation
