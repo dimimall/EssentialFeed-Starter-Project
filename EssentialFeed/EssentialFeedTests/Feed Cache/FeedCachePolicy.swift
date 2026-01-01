e579b16e123112a63a60d8502dda7ee0ddee3641
dimimall <malliaroy@gmail.com>
2026-01-01 12:11:13 +0200

    Move FeedCachePolicy to its own file

---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,8 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeedTests
+//
+//  Created by Dimitra Malliarou on 1/1/26.
+//
+
+import Foundation
---
+++ EssentialFeed/EssentialFeedTests/Feed Cache/LocalFeedLoader.swift
@@ -0,0 +1,8 @@
+//
+//  LocalFeedLoader.swift
+//  EssentialFeedTests
+//
+//  Created by Dimitra Malliarou on 1/1/26.
+//
+
+import Foundation
