b7222011e62dd263bf63940cd062166d5f0a9f26
dimimall <malliaroy@gmail.com>
2026-04-29 16:07:39 +0300

    Implement generic RemoteLoader

--- EssentialFeed/EssentialFeedTests/Shared Api Infra/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Shared Api Infra/URLSessionHTTPClientTests.swift
@@ -37,9 +37,10 @@ class URLSessionHTTPClientTests: XCTestCase {
 	func test_getFromURL_failsOnRequestError() {
 		let requestError = anyNSError()
 		
-		let receivedError = resultErrorFor((data: nil, response: nil, error: requestError))
+		let receivedError = resultErrorFor((data: nil, response: nil, error: requestError)) as NSError?
 		
-		XCTAssertEqual(receivedError as NSError?, requestError)
+        XCTAssertEqual(receivedError?.domain, anyNSError().domain)
+        XCTAssertEqual(receivedError?.code, anyNSError().code)
 	}
 	
 	func test_getFromURL_failsOnAllInvalidRepresentationCases() {
