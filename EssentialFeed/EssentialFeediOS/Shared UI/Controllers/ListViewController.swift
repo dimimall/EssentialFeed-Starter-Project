bef719447b8cdfe6ac6511038c746a025f9a979d
dimimall <malliaroy@gmail.com>
2026-06-10 18:39:14 +0300

    Propagate willDisplayCell delegate method calls

--- EssentialFeed/EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeed/EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -94,6 +94,11 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
             dsp?.tableView?(tableView, cancelPrefetchingForRowsAt: [indexPath])
         }
 	}
+    
+    public override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let dl = cellController(at: indexPath)?.delegate
+        dl?.tableView?(tableView, willDisplay: cell, forRowAt: indexPath)
+    }
 
     private func cellController(at indexPath: IndexPath) -> CellController? {
         dataSource.itemIdentifier(for: indexPath)
